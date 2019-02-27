9093a607bd38ce815c85332deb4add0cb94f3193
agbeterseer <agbe.terseer@gmail.com>
2019-02-27 12:01:50 +0100

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -61,14 +61,6 @@ class RegisterController extends Controller
      * @return \Illuminate\Contracts\Validation\Validator
      */
 
-    // public function index(Request $request, User $user)
-    // {
-    //     //dd($request->all());
-      
-    // return view('register',array('user' => Auth::user()));
-    // }
- 
-
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -84,65 +76,22 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return User
      */
-
-    //   public function create(array $data) {
-
-    //     // try{
-    //     //         DB::transaction(function () use ($data)  {
-              
-    //     //             $user = User::create([
-    //     //                 'name' => $data['name'],
-    //     //                 'email' => $data['email'],
-    //     //                 'password' => bcrypt($data['password']),
-    //     //             ]);
-
-    //     //             $user->attachRole(Role::where('name','general-user')->first());
-
-    //     //             Session::flash('success','User has been Created successfully');
-
-    //     //              return $user;
-
-    //     //      });
-
-    //     //  } catch (\Exception $e) {
-    //     //    return redirect()->back()
-    //     //       ->withErrors(['error' => $e->getMessage()]);
-    //     // }
-
-    // }
-
-    public function create(array $data) { 
  
-    //dd($data);
-
+    public function create(array $data) { 
       $user = User::firstOrNew(['email'=>$data['email']]);
       $user->name = $data['name'];
       $user->password = bcrypt($data['password']);
       $user->account_type = $data['account_type'];
       $user->save();
 
-
-      //create([ 'name' => $data['name'], 'email' => $data['email'], 'password' => bcrypt($data['password']), 'account_type' => $data['account_type']]);
-      // $user = User::findOrFail($user->id);
-      // $user->firstname = $data['firstname'];
-      // $user->lastname = $data['lastname'];
-      // $user->account_type = $data['account_type'];
-      // $user->save(); 
-      // registering client
       if ($data['account_type'] === 'employer') {
     $client = Client::firstOrNew(['name'=> $data['firstname']]);
     $client->created_at = Carbon::now();
     $client->user_id = $user->id;
     $client->save();
-
-
-
-
       }
 
      $recruit_profile_pix = DB::table('recruit_profile_pixs')->insert(['user_id' => $user->id, 'order' => 1, 'status' => 1, 'created_at' => $this->returnCurrentTime()]);
-
- 
      return $user; 
 
     }
