03dbb5ae7d8f55a580f787f7c1d48e1318640174
flints007 <agbe.terseer@gmail.com>
2019-03-07 15:43:45 +0100

    lookups

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
-
+use Illuminate\Http\Request; 
+use Socialite; 
+use App\User;
 class LoginController extends Controller
 {
     /*
