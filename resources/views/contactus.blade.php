dcfcf367d29a6b608363a93eba7bedb1bfcf177a
flints007 <agbe.terseer@gmail.com>
2019-03-13 11:32:52 +0100

    fix address

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -42,7 +42,7 @@
                           <div class="space">&nbsp;</div>  
                           <div class="row">
                             <div class="col-md-3"> <label>Street Name :</label></div>
-                            <div class="col-md-3">{{$cont->street_name}}<br></div>
+                            <div class="col-md-8">{{$cont->street_name}}<br></div>
                           </div>
                           <div class="space">&nbsp;</div>  
                           <div class="row">
