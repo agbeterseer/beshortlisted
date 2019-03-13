b9221a2c2d48347f37fb4ecb1a9e952d2babf866
flints007 <agbe.terseer@gmail.com>
2019-03-13 14:27:48 +0100

    fixes

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -2185,9 +2185,12 @@ return $get_Job_by_common_industries;
 // Job Application 
     public function JobApplicationForm($id){
     //$id = 28;
+
       $user = Auth::user();
        $user_single_resume_by_date = RecruitResume::where('user_id', $user->id)->where('status', 1)->first();
-      if ($user_single_resume_by_date) {
+        
+      if ($user_single_resume_by_date === null) {
+      
       return redirect()->route('show.cation');
       }
     if ($user->account_type === 'employee') {
