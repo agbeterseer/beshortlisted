969186dccf3ddbe30f8b1da0461c6f66e549a1ab
flints007 <agbe.terseer@gmail.com>
2019-03-18 18:06:48 +0100

    employer breadcromb

--- resources/views/partials/employer_breadcomb.blade.php
+++ resources/views/partials/employer_breadcomb.blade.php
@@ -4,14 +4,14 @@
             <div class="careerfy-breadcrumb" style="margin-top: 0px;">
             <!--     <span ><div style="margin-bottom: -190px;"></div><a href="#">CV BANK</a> </span> -->
                 <ul>
-                <li style="background-color: orange;"><span><a href="{{route('list.all')}}">CV BANK</a> </span></li> 
+                <!-- <li style="background-color: orange;"><span><a href="{{route('list.all')}}">CV BANK</a> </span></li>  -->
                    <li><a href="{{route('dashboard')}}">DASHBOARD</a></li>
                     <li><a href="{{route('employer.edit', Auth::user()->id )}}"> PROFILE</a></li> 
                     <li><a href="{{route('manage.jobs')}}">MANAGE JOBS</a></li> 
                     <li><a href="{{route('dashboard')}}">SHORTLIST</a></li> 
                      <li><a href="{{route('employer.edit', Auth::user()->id )}}">SETTINGS</a></li> 
                     <li><a href="{{route('post.jobs')}}"> POST A JOB</a></li> 
-                    <li>Units<span class="badge" style="background-color: orange">
+                    <li>Units &nbsp;<span class="badge" style="background-color: orange">
                                         @if($units) {{$units->units}} @else 0 @endif 
                                     </span> </li>
                 </ul>
