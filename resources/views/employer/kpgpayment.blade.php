e368cf7e5fdad99be6063bde3cf1ca7f0182c74e
flints007 <agbe.terseer@gmail.com>
2019-07-22 15:18:13 +0100

    paystack payment

--- resources/views/employer/kpgpayment.blade.php
+++ resources/views/employer/kpgpayment.blade.php
@@ -71,7 +71,7 @@
             </p> 
             <input type="hidden" name="email" value="{{$user->email}}"> {{-- required --}}
             <input type="hidden" name="orderID" value="#{{$orderID}}">
-            <input type="hidden" name="amount" value="$package_record->price}}"> {{-- required in kobo --}}
+            <input type="hidden" name="amount" value="{{$package_record->price}}"> {{-- required in kobo --}}
             <input type="hidden" name="quantity" value="{{$package_record->jobs_posting}}">
             <input type="hidden" name="metadata" value="{{ json_encode($array = ['key_name' => 'value',]) }}" > {{-- For other necessary things you want to add to your payload. it is optional though --}}
             <input type="hidden" name="reference" value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
