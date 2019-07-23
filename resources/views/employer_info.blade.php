d604fae66f44cccd47f403ce7815291063ffc27a
flints007 <agbe.terseer@gmail.com>
2019-07-23 15:41:32 +0100

    pricing

--- resources/views/employer_info.blade.php
+++ resources/views/employer_info.blade.php
@@ -165,6 +165,10 @@
                                             <li>No Support</li> -->
                                         </ul></div>
                                         <div class="clearfix"></div>
+                                        
+                                        <div class="space">&nbsp;</div>
+                                        <div class="space">&nbsp;</div>
+                                        <div class="space">&nbsp;</div>
                                         <a href="{{route('employer.payment', $plan->id)}}" class="careerfy-priceplan-style5-btn">Buy now</a>
                                     </div>
                                     </div>
