4917284b2c4b0c94cc2ec8715cfa0b7f8c404339
flints007 <agbe.terseer@gmail.com>
2019-07-26 13:45:46 +0100

    packages

--- resources/views/employer/include_price.blade.php
+++ resources/views/employer/include_price.blade.php
@@ -1,8 +1,8 @@
-<div class="careerfy-priceplan-style5-wrap">
+<div class="careerfy-priceplan-style5-wrap"  style="background-color: #ffffff; ">
       <div class="container"> 
    @foreach($plans as $plan) 
        @if($plan->make_active === 1) 
-                                <div class="col-md-4 active""> 
+                                <div class="col-md-4 active"> 
                                     <div class="careerfy-priceplan-style5" >
                                       @if($plan->make_active === 1) 
 
