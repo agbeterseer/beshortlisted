919e77a2b7a109871101b603d56a038f0cfece45
Agbe Terseer <agbe.terseer@gmail.com>
2019-08-27 15:18:13 +0100

    job history fix

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -9,7 +9,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title> Document management System | Admin Dashboard</title>
+        <title> Beshortlisted | Admin Dashboard</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
